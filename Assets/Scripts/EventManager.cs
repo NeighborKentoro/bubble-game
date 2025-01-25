590f11f345d547dab13c22d4b35558a90f3635c7
XULU-XR1\Kent <13387044+NeighborKentoro@users.noreply.github.com>
2025-01-25 11:27:48 -0600

    Added scoring event and a UI canvas for the scoring - just need something to trigger the events and everything should be good

--- Assets/Scripts/EventManager.cs
+++ Assets/Scripts/EventManager.cs
@@ -5,8 +5,16 @@ public class EventManager : MonoBehaviour
     public delegate void BubblePopAction(int playerId);
     public static event BubblePopAction bubblePopEvent;
 
+    public delegate void GenericPlayerAction(int playerId);
+    public static event GenericPlayerAction scoreEvent;
+
     public static void BubblePop(int playerId)
     {
         bubblePopEvent?.Invoke(playerId);
+    }
+
+    public static void PlayerScored(int playerId)
+    {
+        scoreEvent?.Invoke(playerId);
     }
 }
