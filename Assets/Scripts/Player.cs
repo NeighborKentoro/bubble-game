48883dc38e69bcc522d92e63c87b396c2b6bc39b
nickstrelic <nstrelic@gmail.com>
2025-01-25 21:59:03 -0600

    fish size and player bubble pop

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -26,4 +26,15 @@ public class Player : MonoBehaviour
     {
         
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // if colliding with opponents bubble
+        if (collision.collider.gameObject.layer == 7)
+        {
+            // trigger bubble pop event            
+            EventManager.BubblePop();
+            collision.collider.gameObject.GetComponent<Bubble>().Pop();
+        }
+    }
 }
